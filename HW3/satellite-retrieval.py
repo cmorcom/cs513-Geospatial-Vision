7c2be113ad671b68da28681a4964355eaaee782b
Chris Morcom <morcom01@gmail.com>
2020-03-22 08:02:25 -0500

    fixed switch method. tiles can be downloaded

--- HW3/satellite-retrieval.py
+++ HW3/satellite-retrieval.py
@@ -27,32 +27,25 @@ if (lvl>23 or lvl<1): raise Exception("Invalid Detail Level. Choose 1 to 23.")
 
 #reformat bounding box to go from min->max
 def switch(a,b): a,b = b,a
-if maxX<minX: switch(minX,maxX)
-if maxY<minY: switch(minY,maxY)
+if maxX<minX: minX,maxX = maxX,minX
+if maxY<minY: minY,maxY = maxY,minY
 if v: print("Bounding Box: ({},{}) -> ({},{})".format(minX,minY,maxX,maxY))
-if v: print("\nLevel of Detail =", lvl)
+if v: print("Level of Detail =", lvl)
 
 #took these from the source material given to us
 sinLatMin = sin(radians(minY))
 sinLatMax = sin(radians(maxY))
 
-pXmin = (((minX+180)/360)*256)*(2**lvl)
-pXmax = (((maxX+180)/360)*256)*(2**lvl)
-pYmin = (0.5-log((1+sinLatMin)/(1-sinLatMin), 10)/(4*pi))*256*(2**lvl)
-pYmax = (0.5-log((1+sinLatMax)/(1-sinLatMax), 10)/(4*pi))*256*(2**lvl)
-if (v): print("sinLatMin: {}\nsinLatMax: {}\npXmin: {}\npXmax: {}\npYmin: {}\npYmax: {}".format(sinLatMin, sinLatMax, pXmin, pXmax, pYmin, pYmax))
+pXmin = (((minX+180)/360)*256)*(2**lvl); pYmin = (0.5-log((1+sinLatMin)/(1-sinLatMin))/(4*pi))*256*(2**lvl)
+pXmax = (((maxX+180)/360)*256)*(2**lvl); pYmax = (0.5-log((1+sinLatMax)/(1-sinLatMax))/(4*pi))*256*(2**lvl)
+if (v): print("\nsinLatMin: {}\nsinLatMax: {}\npXmin: {}\npXmax: {}\npYmin: {}\npYmax: {}".format(sinLatMin, sinLatMax, pXmin, pXmax, pYmin, pYmax))
 
 #Calc Tile Numbers for each coordinate given c is a pixel coordinate and l is lvl
-tXmin = (int)(pXmin//256)
-tXmax = (int)(pXmax//256)
-tYmin = (int)(pYmin//256)
-tYmax = (int)(pYmax//256)
-if v: print("TileBox: ({},{}) -> ({},{})".format(tXmin,tYmin,tXmax,tYmax))
+tXmin = (int)(pXmin//256); tXmax = (int)(pXmax//256)
+tYmin = (int)(pYmin//256); tYmax = (int)(pYmax//256)
+if tXmax<tXmin: tXmin,tXmax = tXmax, tXmin
+if tYmax<tYmin: tYmin,tYmax = tYmax, tYmin
+
+if v: print("\nTileBox: ({},{}) -> ({},{})".format(tXmin,tYmin,tXmax,tYmax))
 #generate array of tiles that we need
-tiles=[]
-
-for xx in list(range(tXmin, tXmax+1)):
-	for yy in list(range(tYmin, tYmax+1)):
-		tiles.append(tuple(xx,yy))
-
-if v: print('Tiles:', tiles)
+tiles=[(x,y) for x in range(tXmin, tXmax+1) for y in range(tYmin, tYmax+1)]
